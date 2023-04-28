ff153e1a869b054b6af1ca0988a7b2e2c9adc4cd
sauravmodi <sauravmodi03@gmail.com>
2023-04-28 17:56:25 -0500

    Changes for uploading image.

--- app/screens/Account.js
+++ app/screens/Account.js
@@ -1,15 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { Alert, SafeAreaView, StyleSheet, View } from 'react-native';
 import { styles } from '../components/Styles'
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth, db, dbName } from '../../firebaseConfig';
-import { Text } from 'react-native-elements';
+import { auth, db, dbName, storage } from '../../firebaseConfig';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { Image, Text } from 'react-native-elements';
 import { collection, doc, getDoc, onSnapshot, query, where } from 'firebase/firestore';
+import { launchImageLibrary, launchCamera } from 'react-native-image-picker';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import * as ImagePicker from 'react-native-image-picker';
+import { ImagePickerIOS } from 'react-native';
 
 function Account(props) {
 
     const [user] = useAuthState(auth);
     const [userData, setUser] = useState({});
+
+    const [image, setImage] = useState(null);
+
+    // const ImagePicker = require('react-native-image-picker');
+
+    const onSelectImagePress = () => launchImageLibrary({ mediaType: 'photo' }, onMediaSelect);
+
+    // const ref = storage.ref('images/' + user.uid + '/avatar.png');
+
+    const onMediaSelect = async (media) => {
+        if (!media.didCancel) {
+            console.log("Uploading");
+            const reference = ref(storage, 'images/' + media.assets[0].fileName);
+            uploadBytes(reference, media).then((res) => {
+                console.log(res);
+                Alert.alert("Image uploaded");
+                getDownloadURL(reference).then((res) => {
+                    console.log(res);
+                });
+            }).catch((err) => {
+                console.log(err);
+            });
+
+
+        }
+    };
 
     const loadUserData = async () => {
         const ref = doc(db, dbName, user.uid);
@@ -27,6 +58,10 @@ function Account(props) {
 
     return (
         <SafeAreaView style={[styles.flexContainer, st.background]}>
+            <View style={st.infoWrapper}>
+                <Image source={'https://firebasestorage.googleapis.com/v0/b/todo-bcea1.appspot.com/o/images%2Favatar.png?alt=media&token=e8feb75a-4372-4378-8ab6-03a888d7058b'} />
+                <TouchableOpacity onPress={onSelectImagePress}><Text>Upload Pic</Text></TouchableOpacity>
+            </View>
             <View style={st.infoWrapper}>
                 <Text style={st.labelFont}>First Name:</Text>
                 <Text style={st.infoFont}>{userData.fname}</Text>
